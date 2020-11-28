9c10ee96a6df3b294efb2a1f99d60a91ea3166b0
Steve99-coder <stevenderitu99@gmail.com>
2020-11-29 01:18:31 +0300

    Add the test file for article

--- tests/test_source.py
+++ tests/test_source.py
@@ -1,3 +1,17 @@
 import unittest
 from app.model import Sources
 
+class SourcesTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the Source class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_source = Sources("1234","Bbc","Australia's national carrier Qantas will require future international travelers to prove they have been vaccinated","https://www.bbc.com/news/business","Business","English","UK")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_source,Sources))
+
